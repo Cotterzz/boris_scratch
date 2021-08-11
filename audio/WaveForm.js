2ab363b58f2954fcd5fe7fa0610488925f2bcd86
John Cotterell <Cotterzz@users.noreply.github.com>
2021-08-11 03:10:01 +0100

    first commit

---
+++ audio/WaveForm.js
@@ -0,0 +1,21 @@
+class WaveForm {
+
+	resolution=100;
+	signed=true;
+	top=1;
+	bottom=0;
+	dataPoints=[];
+	cachedDatas={};
+	defaultData=null;
+
+	constructor(data=null, signed=true, resolution=100) {
+
+	}
+	
+	getData(bitrate=44000, frequency=1, time=1){
+		
+	}
+
+}
+
+class 
