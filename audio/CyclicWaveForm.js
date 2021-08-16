257fadf940f881745fb9881f3796ee3df7c5f2a5
John Cotterell <Cotterzz@users.noreply.github.com>
2021-08-16 04:42:31 +0100

    Create CyclicWaveForm.js

---
+++ audio/CyclicWaveForm.js
@@ -0,0 +1,20 @@
+class CyclicWaveForm {
+
+	resolution=100;
+	top=1;
+	bottom=-1;
+	dataPoints=[];
+
+	constructor(preset="sine", resolution=100) {
+
+	}
+	
+	createSamplePoints(){
+
+	}
+
+	createRenderPoints(){
+		
+	}
+
+}
