899ffb6067620c26d6b8859b2f2b36ae4187a697
John Cotterell <Cotterzz@users.noreply.github.com>
2021-11-26 10:23:57 +0000

    Update CyclicWaveForm.js

--- audio/CyclicWaveForm.js
+++ audio/CyclicWaveForm.js
@@ -11,7 +11,7 @@ class CyclicWaveForm {
 	
 	createSamplePoints(){
 
-	}
+	}                                                                                                                               
 
 	createRenderPoints(){
 		
